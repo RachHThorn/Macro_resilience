0a7149b6fc55d74f676411e9785fbe143e1e92ae
Rachael Thornley <rthornley28@gmail.com>
2025-08-31 21:28:11 +0100

    regressions

--- code/weighted_regression.R
+++ code/weighted_regression.R
@@ -3,6 +3,13 @@
 # Regressions for the Life History Traits and RE outcomes
 
 library(tidyverse)
+library(purrr)
+library(broom)
+library(brms)
+
+################################################################################
+# load and tidy data
+################################################################################
 
 # Load demography variables and find the mean and SE of each Taxon and trait
 
@@ -35,8 +42,6 @@ demo %>% group_by(Taxon, Demo_trait) %>% count()
 # save a vector of taxa names to use in the next stage of modelling
 comp_taxa <- unique(demo$Taxon) # 41 taxa
 
-###############################################################################
-
 # read in effect sizes from the whole of DRAGNet 
 taxa <- read_csv("results/RE_SE_Taxon_all_DRAGNet.csv")
 unique(taxa$time_period)
@@ -44,7 +49,7 @@ unique(taxa$time_period)
 # comp_taxa <- read_csv("results/common_species_drag_comp.csv") %>% pull(x)
 
 # select the random effects for the var of interest only (New_taxon)
-# create a new ID variable
+# create a new ID variable the makes a note if data are in the compadre data set
 # change some labels in the data frame to tidy it up
 taxa <- 
   taxa %>% 
@@ -59,7 +64,10 @@ taxa <-
 names(taxa)
 unique(taxa$Taxon) # 40 unique taxa here
 
-###############################################################################
+
+################################################################################
+# OPTION 1: Easy linear models
+################################################################################
 
 # quick and dirty linear regression using just the means of the demo metrics and REs
 
@@ -83,8 +91,7 @@ unique(both$time_period)
 unique(both$experiment)
 
 # very rough crude linear regressions of mean values
-library(purrr)
-library(broom)
+
 both %>% group_by(model, time_period, experiment, Demo_trait) %>% count()
 nested_both <- both %>% group_by(model, time_period, experiment, Demo_trait) %>% nest()
 
@@ -141,8 +148,6 @@ lm_results %>%
 lm_results$p.value
 lm_results %>% filter(p.value < 0.1)
 
-################################################################################
-
 # if we plot the actual data and look at the plots for the Lambda variable at T0-T3
 # data is called both
 names(both)
@@ -157,7 +162,7 @@ both %>%
   ylab("Species cover change effect")
 # this relationship is largely driven by an outlier
 # save this plot as evidence
-ggsave("figures/Lambda_mean_data_plot.jpeg", height = 5, width = 5)
+ggsave("figures/Lambda_mean_data_plot.jpeg", height = 5, width = 7)
 
 unique(both$Demo_trait)
 both %>% 
@@ -169,9 +174,11 @@ both %>%
   
 # this is also very unconvincing
 
-################################################################################
 
-# OPTION 2: weighted regression
+
+################################################################################
+# OPTION 2: Weighted regression
+################################################################################
 # weights as inverse variance
 names()
 df <- df %>%
@@ -179,3 +186,26 @@ df <- df %>%
 
 fit <- lm(y ~ x, data = df, weights = weight)
 
+
+
+
+################################################################################
+# OPTION 3: Bayesian regression
+################################################################################
+
+# here we need a data frame with errors for both 
+
+
+
+
+models_bayes <- nested_both %>%
+  mutate(
+    data = map(data, ~ .x %>%
+                 filter(is.finite(New_demo_value), is.finite(RE)) %>%  # clean numeric columns
+                 drop_na(New_demo_value, RE))),
+mod = map(data, ~ brm(
+  formula = RE ~ New_demo_value, 
+  data = .x,
+  family = gaussian(),
+  chains = 2, iter = 2000, cores = 2,  # adjust for speed/memory
+  silent = TRUE)), results = map(mod, ~ broom.mixed::tidy(.x, effects = "fixed")))
